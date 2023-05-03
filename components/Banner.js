c4b1f87b0c384c7aae656c0dff8f35565426ab75
Lindris <linda01harris@gmail.com>
2023-05-03 12:07:58 +0100

    banner-components done

--- components/Banner.js
+++ components/Banner.js
@@ -1,7 +1,31 @@
 import React from "react";
+import styles from "../src/styles/Banner.module.css"
+
+//create div with header, paragraph and button,
+//wrap this div in another div to enable styling later
+//add another div below 1st closing div for image  - 1st div will be on left, 2nd div on right
+//give divs class names using {} to use with css modules (not CSS)- as it allows ...... 
+//create new css file in Styles
+//import styles from styles/Banner.module.css
+//in {} add styles. before container - to import the css for container because using modules
+//in 2nd div for text - on left side - class name banner_text
+//in 3rd div for image- on right side - class name styles.banner_image
+//use span to separate wording that will be red
+//class name in spans and button for css
+//add css in styles - mobile first
+ 
 
 function Banner() {
-  return <div>Banner</div>;
+  return <div className={styles.container}>
+  <div className={styles.banner_text}>
+    <h1>Touch typing training for 
+children and adults</h1>
+<p><span className={styles.banner_span}>Save time and work smarter </span> with one of the most valuable skills you or your children can learn.</p>
+<p><span className={styles.banner_span} >Touch typing</span> is learning to type quickly and accurately without looking at your hands using all your fingers on specific keys. <span className={styles.banner_span}>Join our course</span> and gain a hugely beneficial skill for life.</p>
+<button className={styles.banner_button}>Our Courses</button>
+</div>
+<div className={styles.banner_image}>Insert Image</div>
+  </div>;
 }
 
 export default Banner;
