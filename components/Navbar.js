90051c16e7fee56ce1a6ba302d08362025d36cae
Lindris <linda01harris@gmail.com>
2023-05-18 11:31:56 +0100

    navbar styled

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import styles from "../src/styles/Navbar.module.css";
+
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className={styles.nav} >
+      <div className={styles.logo} >LOGO</div>
+      <Link className={styles.link_nav} href="/">Home</Link>
+      <Link className={styles.link_nav}  href="/about">About</Link>
+      <Link className={styles.link_nav}  href="/course">Course</Link>
+      <Link className={styles.link_nav}  href="/reviews">Reviews</Link>
+      <Link className={styles.link_nav}  href="/contact">Contact</Link>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
